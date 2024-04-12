3587c1df3020f00959d3427c9e1f7dc60027b2a2
RasmusTufvesson <lars@bizzbook.com>
2024-04-12 17:29:31 +0200

    add moving the mouse

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::{env::args, thread::sleep, time::Duration};
 use enigo::{Key, KeyboardControllable, MouseButton, MouseControllable};
 
 const SLEEP_TIME: Duration = Duration::from_secs(3);
-const DOT_WIDTH: i32 = 7;
+const DOT_WIDTH_FLOAT: f32 = 6.;
+const DOT_WIDTH: i32 = 6;
 
 fn main() {
     let image_path = args().nth(1).expect("Specify image path");
@@ -28,4 +29,14 @@ fn main() {
     enigo.key_click(Key::Z);
     enigo.key_click(Key::UpArrow);
     enigo.key_click(Key::Return);
+    let painting_width = right - left;
+    let painting_height = bottom - top;
+    let horizontal_dots = (painting_width as f32 / DOT_WIDTH_FLOAT).ceil() as i32;
+    let vertical_dots = (painting_height as f32 / DOT_WIDTH_FLOAT).ceil() as i32;
+    for x in 0..=horizontal_dots {
+        for y in 0..=vertical_dots {
+            enigo.mouse_move_to(left + x * DOT_WIDTH, top + y * DOT_WIDTH);
+            enigo.mouse_click(MouseButton::Left);
+        }
+    }
 }
