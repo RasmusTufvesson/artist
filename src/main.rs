13831103b0f6d3923649a562b6fe35469b9dd6c6
RasmusTufvesson <lars@bizzbook.com>
2024-04-12 17:18:37 +0200

    set up drawing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
-use std::env::args;
+use std::{env::args, thread::sleep, time::Duration};
+use enigo::{Key, KeyboardControllable, MouseButton, MouseControllable};
 
-use image::GenericImageView;
+const SLEEP_TIME: Duration = Duration::from_secs(3);
+const DOT_WIDTH: i32 = 7;
 
 fn main() {
-    if let Some(image_path) = args().nth(1) {
-        let img = image::open(image_path).unwrap();
-        println!("{:?}", img.get_pixel(0, 0));
-    }
+    let image_path = args().nth(1).expect("Specify image path");
+    let img = image::open(image_path).expect("Could not open image");
+    let mut enigo = enigo::Enigo::new();
+    sleep(SLEEP_TIME);
+    let (left, top) = enigo.mouse_location();
+    enigo.mouse_click(MouseButton::Left);
+    sleep(SLEEP_TIME);
+    let (right, bottom) = enigo.mouse_location();
+    enigo.mouse_click(MouseButton::Left);
+    sleep(SLEEP_TIME);
+    let (black_x, black_y) = enigo.mouse_location();
+    enigo.key_down(Key::Control);
+    enigo.key_down(Key::A);
+    enigo.key_up(Key::Control);
+    enigo.key_up(Key::A);
+    enigo.key_click(Key::Delete);
+    enigo.key_click(Key::Alt);
+    enigo.key_click(Key::B);
+    enigo.key_click(Key::Alt);
+    enigo.key_click(Key::S);
+    enigo.key_click(Key::Z);
+    enigo.key_click(Key::UpArrow);
+    enigo.key_click(Key::Return);
 }
