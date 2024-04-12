1e020abdd28f8828ad094db2a972490888caa418
RasmusTufvesson <lars@bizzbook.com>
2024-04-12 19:02:09 +0200

    add colors

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::{env::args, thread::sleep, time::Duration};
 use enigo::{Key, KeyboardControllable, MouseButton, MouseControllable};
-use image::DynamicImage;
+use image::{DynamicImage, GenericImageView, Pixel, Rgb};
 
 const SLEEP_TIME: Duration = Duration::from_secs(3);
+const SMALL_SLEEP_TIME: Duration = Duration::from_millis(5);
 const DOT_WIDTH_FLOAT: f32 = 5.;
 const DOT_WIDTH: i32 = 5;
+const COLOR_SPACING: i32 = 26;
 
 fn main() {
     let image_path = args().nth(1).expect("Specify image path");
     let img = image::open(image_path).expect("Could not open image");
-    let mut artist = Artist::new(img);
+    let colors = [*Rgb::from_slice(&[0,0,0]), *Rgb::from_slice(&[127,127,127]), *Rgb::from_slice(&[136,0,21]),
+                                  *Rgb::from_slice(&[237,28,36]), *Rgb::from_slice(&[255,127,39]), *Rgb::from_slice(&[255,242,0]),
+                                  *Rgb::from_slice(&[34,177,76]), *Rgb::from_slice(&[0,162,232]), *Rgb::from_slice(&[63,72,204]),
+                                  *Rgb::from_slice(&[163,73,164]), *Rgb::from_slice(&[255,255,255]), *Rgb::from_slice(&[195,195,195]),
+                                  *Rgb::from_slice(&[185,122,87]), *Rgb::from_slice(&[255,174,201]), *Rgb::from_slice(&[255,201,14]),
+                                  *Rgb::from_slice(&[239,228,176]), *Rgb::from_slice(&[181,230,29]), *Rgb::from_slice(&[153,217,234]),
+                                  *Rgb::from_slice(&[112,146,190]), *Rgb::from_slice(&[200,191,231])];
+    let mut artist = Artist::new(img, colors);
     artist.paint();
 }
 
@@ -22,10 +31,13 @@ struct Artist {
     black_y: i32,
     width: i32,
     height: i32,
+    colors: [Rgb<u8>; 20],
+    selected_color: i32,
+    canvas_selected: bool,
 }
 
 impl Artist {
-    fn new(img: DynamicImage) -> Self {
+    fn new(img: DynamicImage, colors: [Rgb<u8>; 20]) -> Self {
         let mut enigo = enigo::Enigo::new();
         sleep(SLEEP_TIME);
         let (left, top) = enigo.mouse_location();
@@ -51,25 +63,66 @@ impl Artist {
         let painting_height = bottom - top;
         let horizontal_dots = (painting_width as f32 / DOT_WIDTH_FLOAT).ceil() as i32;
         let vertical_dots = (painting_height as f32 / DOT_WIDTH_FLOAT).ceil() as i32;
-        Self { enigo, img, left, top, black_x, black_y, width: horizontal_dots, height: vertical_dots }
+        Self { enigo, img, left, top, black_x, black_y, width: horizontal_dots, height: vertical_dots, colors, selected_color: 0, canvas_selected: false }
     }
 
     fn paint(&mut self) {
-        self.draw_dot(0, 0);
         for x in 0..=self.width {
             for y in 0..=self.height {
-                let (canvas_x, canvas_y) = (x * DOT_WIDTH, y * DOT_WIDTH);
-                self.draw_dot(canvas_x, canvas_y);
+                self.pick_color(x, y);
+                self.draw_dot(x * DOT_WIDTH, y * DOT_WIDTH);
             }
         }
     }
 
     fn click(&mut self, x: i32, y: i32) {
+        sleep(SMALL_SLEEP_TIME);
         self.enigo.mouse_move_to(x, y);
         self.enigo.mouse_click(MouseButton::Left);
     }
 
     fn draw_dot(&mut self, x: i32, y: i32) {
+        if !self.canvas_selected {
+            self.click(self.left + x, self.top + y);
+            self.canvas_selected = true;
+        }
         self.click(self.left + x, self.top + y);
     }
+
+    fn pick_color(&mut self, x: i32, y: i32) {
+        let img_x = (x as f32 / self.width as f32 * (self.img.width() - 1) as f32).round() as u32;
+        let img_y = (y as f32 / self.height as f32 * (self.img.height() - 1) as f32).round() as u32;
+        let color: Rgb<u8> = self.img.get_pixel(img_x, img_y).to_rgb();
+        let color_index = self.get_best_color(color);
+        self.select_color(color_index);
+    }
+
+    fn get_best_color(&self, color: Rgb<u8>) -> i32 {
+        let mut best_match: i32 = 0;
+        let mut best_match_value = f32::INFINITY;
+        for i in 0..self.colors.len() {
+            let diff = color_difference(color, self.colors[i]);
+            if diff < best_match_value {
+                best_match = i as i32;
+                best_match_value = diff;
+            }
+        }
+        best_match
+    }
+
+    fn select_color(&mut self, color_index: i32) {
+        if self.selected_color != color_index {
+            let column = color_index % 10;
+            let row = (color_index - column) / 10;
+            let x = column * COLOR_SPACING + self.black_x;
+            let y = row * COLOR_SPACING + self.black_y;
+            self.click(x, y);
+            self.selected_color = color_index;
+            self.canvas_selected = false;
+        }
+    }
+}
+
+fn color_difference(color_1: Rgb<u8>, color_2: Rgb<u8>) -> f32 {
+    ((color_1.0[0] as f32 - color_2.0[0] as f32).abs() + (color_1.0[1] as f32 - color_2.0[1] as f32).abs() + (color_1.0[2] as f32 - color_2.0[2] as f32).abs()).sqrt()
 }
